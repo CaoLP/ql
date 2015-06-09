21d61413919ca6d445b6e15ae64fe8e04aec07d4
lpcao <caolpgc60067@gmail.com>
2015-06-09 16:12:19 +0700

    inventory

--- app/Controller/ReportsController.php
+++ app/Controller/ReportsController.php
@@ -180,7 +180,210 @@ class ReportsController extends AppController
 
     public function admin_product_in_warehouse()
     {
+        $conditions = array();
+        $store_id = 1;
+        $summary = array(
+            'before_total' => 0,
+            'before_price' => 0,
+            'ship' => 0,
+            'in_qty' => 0,
+            'in_price' => 0,
+            'out_qty' => 0,
+            'out_price' => 0,
+            'sale_qty' => 0,
+            'sale_promote' => 0,
+            'sale_price' => 0,
+            'after_total' => 0,
+            'after_price' => 0,
+            'profit' => 0,
+        );
+        $date = array(date('Y-m-01 00:00'), date('Y-m-t 23:59'));
+        if (isset($this->request->data['store_id']) && !empty($this->request->data['store_id'])) {
+            $store_id = $this->request->data['store_id'];
+            $conditions['Warehouse.store_id'] = $store_id;
+        }
+        if (!empty($this->request->data['from']) && !empty($this->request->data['to'])) {
+            $date = array(
+                $this->request->data['from'] . ' 00:00:00',
+                $this->request->data['to'] . ' 23:59:59'
+            );
+        } else if (!empty($this->request->data['from'])) {
+            $date = array(
+                $this->request->data['from'] . ' 00:00:00',
+                $this->request->data['from'] . ' 23:59:59'
+            );
+        } else if (!empty($this->request->data['to'])) {
+            $date = array(
+                $this->request->data['to'] . ' 00:00:00',
+                $this->request->data['to'] . ' 23:59:59'
+            );
+        }
+        $this->loadModel('Warehouse');
+        $products = $this->Warehouse->find('all', array(
+            'fields' => array(
+                'Warehouse.product_id',
+                'Sum(Warehouse.qty) as total',
+                'Product.name',
+                'Product.sku',
+                'Product.price',
+                'Product.category_id',
+                'Product.retail_price',
+                'Product.source_price',
+            ),
+            'recursive' => 1,
+            'conditions' => $conditions,
+            'group' => array('Warehouse.product_id'),
+            'order' => array('total' => 'desc'),
+        ));
+        $this->loadModel('WarehouseLog');
+        $warehouse_logs = $this->WarehouseLog->find('all', array(
+            'fields' => array('WarehouseLog.product_id','WarehouseLog.qty'),
+            'joins' => array(
+                array(
+                    'table' => "(SELECT warehouse_id , MAX(created) as max_created
+						from warehouse_logs
+						where created < '" . $date[0] . "' AND store_id = " . $store_id . "
+						group by warehouse_id)",
+                    'type' => 'INNER',
+                    'alias' => 'MaxDate',
+                    'conditions' => array(
+                        'MaxDate.max_created = WarehouseLog.created',
+                        'MaxDate.warehouse_id = WarehouseLog.warehouse_id',
+                    )
+                )
+            )
+        ));
+        $warehouse_logs = Set::combine($warehouse_logs, '{n}.WarehouseLog.product_id', '{n}.WarehouseLog');
+        $array_rebuild = array();
+        $this->loadModel('Store');
+        $stores = $this->Store->find('list');
+        $this->loadModel('OrderDetail');
+        $order_products = $this->OrderDetail->find('all', array(
+            'fields' => array(
+                'OrderDetail.product_id',
+                'Sum(OrderDetail.qty) as qty',
+                'Sum(OrderDetail.qty * OrderDetail.price) as price',
+                'Sum(OrderDetail.qty * OrderDetail.promote_value) as promote',
+            ),
+            'conditions' => array(
+                'Order.created between ? and ?' => $date,
+                'Order.status' => 1,
+                'Order.store_id' => $store_id,
+            ),
+            'group' => array(
+                'OrderDetail.product_id'
+            )
+        ));
+        $order_products = Set::combine($order_products, '{n}.OrderDetail.product_id', '{n}.0');
+        $orders = $this->OrderDetail->Order->find('all', array(
+            'fields' => array(
+                'Sum(Order.total_promote) as promote',
+            ),
+            'conditions' => array(
+                'Order.created between ? and ?' => $date,
+                'Order.status' => 1,
+                'Order.store_id' => $store_id,
+            )
+        ));
+        if (isset($orders[0][0]['promote']))
+            $summary['sale_promote'] = $orders[0][0]['promote'];
+//        debug($orders);die;
 
+        $this->loadModel('InoutWarehouseDetail');
+        $in = $this->InoutWarehouseDetail->find('all', array(
+            'fields' => array(
+                'InoutWarehouseDetail.product_id',
+                'Sum(InoutWarehouseDetail.qty) as qty',
+                'Sum(InoutWarehouseDetail.qty * InoutWarehouseDetail.price) as price',
+            ),
+            'conditions' => array(
+                'InoutWarehouse.approved between ? and ?' => $date,
+                'InoutWarehouse.status' => 1,
+                'InoutWarehouse.type' => 0,
+                'InoutWarehouse.store_id' => $store_id,
+            ),
+            'group' => array(
+                'InoutWarehouseDetail.product_id'
+            )
+        ));
+        $in = Set::combine($in, '{n}.InoutWarehouseDetail.product_id', '{n}.0');
+        $out = $this->InoutWarehouseDetail->find('all', array(
+            'fields' => array(
+                'InoutWarehouseDetail.product_id',
+                'Sum(InoutWarehouseDetail.qty_received) as qty',
+                'Sum(InoutWarehouseDetail.qty_received * InoutWarehouseDetail.price) as price',
+            ),
+            'conditions' => array(
+                'InoutWarehouse.approved between ? and ?' => $date,
+                'InoutWarehouse.status' => 1,
+                'InoutWarehouse.type' => 1,
+                'InoutWarehouse.store_id' => $store_id,
+            ),
+            'group' => array(
+                'InoutWarehouseDetail.product_id'
+            )
+        ));
+        $out = Set::combine($out, '{n}.InoutWarehouseDetail.product_id', '{n}.0');
+        foreach ($products as $k => $p) {
+            $array_rebuild[$p['Warehouse']['product_id']] = array(
+                'code' => $p['Product']['sku'],
+                'name' => $p['Product']['name'],
+                'price' => $p['Product']['price'],
+                'retail_price' => $p['Product']['retail_price'],
+                'source_price' => $p['Product']['source_price'],
+                'before_total' => 0,
+                'in_qty' => 0,
+                'in_price' => 0,
+                'out_qty' => 0,
+                'out_price' => 0,
+                'sale_qty' => 0,
+                'sale_promote' => 0,
+                'sale_price' => 0,
+                'after_total' => $p[0]['total'],
+                'profit' => 0,
+                'category_id' => $p['Product']['category_id'],
+            );
+            $summary['after_total'] += $p[0]['total'];
+            $summary['after_price'] += $p[0]['total'] * $p['Product']['price'] ;
+            if (isset($order_products[$p['Warehouse']['product_id']])) {
+                $array_rebuild[$p['Warehouse']['product_id']]['sale_qty'] = $order_products[$p['Warehouse']['product_id']]['qty'];
+                $array_rebuild[$p['Warehouse']['product_id']]['sale_promote'] = $order_products[$p['Warehouse']['product_id']]['promote'];
+                $array_rebuild[$p['Warehouse']['product_id']]['sale_price'] = $order_products[$p['Warehouse']['product_id']]['price'];
+                $summary['sale_qty'] += $order_products[$p['Warehouse']['product_id']]['qty'];
+                $summary['sale_price'] += $order_products[$p['Warehouse']['product_id']]['price'];
+            }
+            if (isset($in[$p['Warehouse']['product_id']])) {
+                $array_rebuild[$p['Warehouse']['product_id']]['in_qty'] = $in[$p['Warehouse']['product_id']]['qty'];
+                $array_rebuild[$p['Warehouse']['product_id']]['in_price'] = $in[$p['Warehouse']['product_id']]['price'];
+                $summary['in_qty'] += $in[$p['Warehouse']['product_id']]['qty'];
+                $summary['in_price'] += $in[$p['Warehouse']['product_id']]['price'];
+            }
+            if (isset($out[$p['Warehouse']['product_id']])) {
+                $array_rebuild[$p['Warehouse']['product_id']]['out_qty'] = $out[$p['Warehouse']['product_id']]['qty'];
+                $array_rebuild[$p['Warehouse']['product_id']]['out_price'] = $out[$p['Warehouse']['product_id']]['price'];
+                $summary['out_qty'] += $out[$p['Warehouse']['product_id']]['qty'];
+                $summary['out_price'] += $out[$p['Warehouse']['product_id']]['price'];
+            }
+            if(isset($warehouse_logs[$p['Warehouse']['product_id']])){
+                $array_rebuild[$p['Warehouse']['product_id']]['before_total'] = $warehouse_logs[$p['Warehouse']['product_id']]['qty'];
+                $summary['before_total'] += $warehouse_logs[$p['Warehouse']['product_id']]['qty'];
+                $summary['before_price'] += $array_rebuild[$p['Warehouse']['product_id']]['before_total'] * $p['Product']['price'];
+            }else{
+                $array_rebuild[$p['Warehouse']['product_id']]['before_total'] =
+                    $array_rebuild[$p['Warehouse']['product_id']]['after_total']
+                    - $array_rebuild[$p['Warehouse']['product_id']]['in_qty']
+                    + $array_rebuild[$p['Warehouse']['product_id']]['out_qty']
+                    + $array_rebuild[$p['Warehouse']['product_id']]['sale_qty'];
+                $summary['before_total'] += $array_rebuild[$p['Warehouse']['product_id']]['before_total'];
+                $summary['before_price'] +=  $array_rebuild[$p['Warehouse']['product_id']]['before_total'] * $p['Product']['price'];
+            }
+        }
+//        die;
+        $categories = Set::combine($array_rebuild,'{n}.code','{n}','{n}.category_id');
+        $this->loadModel('Category');
+        $cats = $this->Category->find('list');
+        $this->set('title_for_layout', 'Báo cáo Nhập-Xuất-Tồn từ ' . $date[0] . ' đến ' . $date[1]);
+        $this->set(compact('categories', 'stores', 'order_products', 'date', 'summary','cats'));
     }
 
     public function admin_product_underlimited()
